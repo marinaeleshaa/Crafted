c3daba38e276b4bca55c08ea59f86596b9f8325b
marinaeleshaa <marinaeleshah77@gmail.com>
2026-01-12 18:34:42 +0200

    refactor: remove unused import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/shared/navbar/navbar';
-import { ModeToggle } from './components/shared/mode-toggle/mode-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ModeToggle, Navbar],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
