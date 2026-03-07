f44f6605f732ea805fd306d5f75faf2dfc4fe0ec
marinaeleshaa <marinaeleshah77@gmail.com>
2026-03-07 15:52:33 +0200

    fix: update API base URL to production environment

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,5 @@
 export const environment  = {
   production: false,                     // عشان تعرفي لو build لل production
-  apiBaseUrl: 'http://localhost:5000/api' // الـ base URL للـ backend
+  apiBaseUrl: 'https://crafted-back.vercel.app/api' // الـ base URL للـ backend
+  // apiBaseUrl: 'http://localhost:5000/api' // الـ base URL للـ backend
 };
